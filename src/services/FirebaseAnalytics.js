254ec16d0017d4735d84d0e6bc14ac922adc43ea
Carlos Villalobos <charliemike31245@gmail.com>
2023-01-26 10:13:33 -0500

    test

--- src/services/FirebaseAnalytics.js
+++ src/services/FirebaseAnalytics.js
@@ -2,6 +2,7 @@ import { App } from "@/firebase.config";
 import { logEvent, getAnalytics } from "firebase/analytics";
 
 const analytics = getAnalytics(App);
+let test = 2;
 
 function LogCustomEvent(eventName, params) {
     logEvent(analytics, eventName, params);
